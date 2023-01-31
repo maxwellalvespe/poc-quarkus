f7f6bd871349e0fce80e9fce5fa17500338371c1
maxwellalvespe <=>
2023-01-30 22:33:00 -0300

    adicionado teste para chamada do recurso /api/users #deployuat

--- src/test/java/org/acme/UsersTest.java
+++ src/test/java/org/acme/UsersTest.java
@@ -1,6 +1,8 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -8,13 +10,12 @@ import static io.restassured.RestAssured.given;
 @QuarkusTest
 public class UsersTest {
 
-    // @Test
-    // public void testHelloEndpoint() {
-    //     given()
-    //       .when().get("/users")
-    //       .then()
-    //          .statusCode(200);
-    //          //.body(is("{"nome":"maxwell"}"));
-    // }
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/api/users")
+          .then()
+             .statusCode(200);
+    }
 
 }
